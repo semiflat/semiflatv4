75561c5fd01a234def0d99bb2aafe1c65a41ae39
kortykotropina <kortykotropina@gmail.com>
2023-02-02 13:52:25 +0100

    Added gradient animation

--- src/components/sections/index/TheHeroSection.vue
+++ src/components/sections/index/TheHeroSection.vue
@@ -12,73 +12,75 @@ import YuzedataLogo from '~/assets/logos/yuzedata.svg?component'
 import { useBreakpoints } from '~/composables/useBreakpoints'
 
 const logos = [
-    OdonataLogo,
-    SisuLogo,
-    PhenomLogo,
-    DefaultLogo,
-    ClickmagicLogo,
-    PocusLogo,
-    YuzedataLogo
+  OdonataLogo,
+  SisuLogo,
+  PhenomLogo,
+  DefaultLogo,
+  ClickmagicLogo,
+  PocusLogo,
+  YuzedataLogo,
 ]
 
 const breakpoints = useBreakpoints()
 const swiperBreakpoints = {
-    [breakpoints.lg]: {
-        slidesPerView: 'auto',
-        watchSlidesVisibility: true,
-
-    },
+  [breakpoints.lg]: {
+    slidesPerView: 'auto',
+    watchSlidesVisibility: true,
+  },
 }
 
-
 const swiperOptions = computed(() => ({
-    slidesPerView: 'auto',
-    //   spaceBetween: 16,
-    navigation: false,
-    loop: true,
-    watchSlidesVisibility: true,
-    autoplay: {
-        delay: 2000,
-    },
-    breakpoints: swiperBreakpoints,
+  slidesPerView: 'auto',
+  //   spaceBetween: 16,
+  navigation: false,
+  loop: true,
+  watchSlidesVisibility: true,
+  autoplay: {
+    delay: 2000,
+  },
+  breakpoints: swiperBreakpoints,
 }))
-
 </script>
 
 <template>
-    <div class="relative page-content">
-        <BackgroundMobileLeft class="absolute pointer-events-none top-28 left-0 md:hidden" />
-        <BackgroundDesktop class="hidden pointer-events-none absolute -left-42 md:block -top-60" />
-        <div class=" mt-3.5rem md:mt-40 md:text-center">
-            <h1 class="hero-title md:text-[2.5rem] md:leading-[120%]  md:mt-6">Product design for SaaS startups_</h1>
-            <p class="mt-6 text-blue-200 ">Semiflat is a full-service design agency specialized in solving complex
-                problems through design. <br /> We design tools that help millions of professionals
-                be more effective at their job.</p>
-            <div class="mt-2.5rem">
-                <AppButton>Discuss your project</AppButton>
-                <AppButton text>See our work</AppButton>
-            </div>
-        </div>
-        <div class="">
-            <AppSlider
-class="mt-16 md:mt-40 " style="" :swiper-options="swiperOptions">
-                <SwiperSlide
-v-for="(logo, i) in logos" :key="i"
-                    class="w-[180px] md:w-[240px] shrink-0 mx-auto flex items-center justify-center">
-                    <component :is="logo" class="h-[38px] grayscale" />
-                </SwiperSlide>
-            </AppSlider>
-        </div>
+  <div class="relative page-content">
+    <BackgroundMobileLeft class="absolute pointer-events-none top-28 left-0 md:hidden" />
+    <BackgroundDesktop class="hidden pointer-events-none absolute -left-42 md:block -top-60" />
+    <div class="mt-3.5rem md:mt-40 md:text-center">
+      <h1 class="hero-title md:text-[2.5rem] md:leading-[120%] md:mt-6">
+        Product design for SaaS startups_
+      </h1>
+      <p class="mt-6 text-blue-200">
+        Semiflat is a full-service design agency specialized in solving complex problems through
+        design. <br />
+        We design tools that help millions of professionals be more effective at their job.
+      </p>
+      <div class="mt-2.5rem">
+        <AppButton>Discuss your project</AppButton>
+        <AppButton text>See our work</AppButton>
+      </div>
     </div>
+    <div class="">
+      <AppSlider class="mt-16 md:mt-40" style="" :swiper-options="swiperOptions">
+        <SwiperSlide
+          v-for="(logo, i) in logos"
+          :key="i"
+          class="w-[180px] md:w-[240px] shrink-0 mx-auto flex items-center justify-center"
+        >
+          <component :is="logo" class="h-[38px] grayscale" />
+        </SwiperSlide>
+      </AppSlider>
+    </div>
+  </div>
 </template>
 
 <style>
 .hero-title {
-    @apply text-[1.75rem] font-semibold leading-[34px] gradient-text;
+  @apply text-[1.75rem] font-semibold leading-[34px] gradient-text;
 }
 
 .swiper {
-    mask-image: linear-gradient(
+  mask-image: linear-gradient(
     270deg,
     rgba(242, 242, 242, 0) 0%,
     rgba(245, 245, 245, 1) 15%,
@@ -93,7 +95,6 @@ v-for="(logo, i) in logos" :key="i"
     rgba(255, 255, 255, 1) 50%,
     rgba(244, 244, 244, 1) 85%,
     rgba(242, 242, 242, 0) 100%
-  ) 
+  );
 }
-
-</style> 
+</style>
--- src/renderer/PageShell.vue
+++ src/renderer/PageShell.vue
@@ -1,17 +1,14 @@
-<script setup lang="ts">
-
-// import BackgroundTurquoise from '~/assets/bg-turquoise.svg?component'
-// import BackgroundMoblieLeft from '~/assets/bg-hero-left-mobile.svg?component' blur-[120px]
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="layout overflow-hidden">
     <div class="page-content relative">
       <div
-        class="bg-turquoise-100/80 pointer-events-none rounded-full blur-[120px] h-[28.125rem] w-[28.125rem] absolute -left-50 -top-18 md:w-[43.75rem] md:h-[43.75rem] md:-left-90 md:top-26 md:blur-[240px]" />
+        class="bg-gradient bg-gradient-left bg-turquoise-100/80 pointer-events-none rounded-full blur-[120px] h-[28.125rem] w-[28.125rem] absolute -left-50 -top-18 md:w-[43.75rem] md:h-[43.75rem] md:-left-90 md:top-26 md:blur-[240px]"
+      />
       <div
-        class="bg-turquoise-200 pointer-events-none rounded-full blur-[120px] h-[12.5rem] w-[12.5rem] absolute -right-15 -top-17 md:h-[18.75rem] md:w-[18.75rem] md:blur-200px" />
+        class="bg-gradient bg-gradient-right bg-turquoise-200 pointer-events-none rounded-full blur-[120px] h-[12.5rem] w-[12.5rem] absolute -right-15 -top-17 md:h-[18.75rem] md:w-[18.75rem] md:blur-200px"
+      />
     </div>
     <TheLayoutNavbar />
     <div class="content">
@@ -20,3 +17,22 @@
     <TheLayoutFooter />
   </div>
 </template>
+
+<style lang="postcss" scoped>
+.bg-gradient {
+  animation: gradient-scale 4s ease-in-out infinite alternate;
+}
+
+.bg-gradient-right {
+  animation: gradient-scale 6s ease-in-out infinite alternate;
+}
+
+@keyframes gradient-scale {
+  0% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
