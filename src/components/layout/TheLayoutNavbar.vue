44d5c6d6249f719072d1aac42a72b16ae4c3244e
Artur Burecki <a.burecki@standardweryfikacji.pl>
2023-02-09 20:12:30 +0100

    navbar links working

--- src/components/layout/TheLayoutNavbar.vue
+++ src/components/layout/TheLayoutNavbar.vue
@@ -37,7 +37,13 @@ const pageContext = usePageContext()
         <LogoDesktop class="hidden md:block" />
         <LogoMobile class="md:hidden" />
         <ul class="gap-6 text-sm hidden md:flex text-primary font-semibold">
-          <li v-for="link in links" :key="link.path" class="cursor-pointer py-1.5 px-4 rounded-[8rem]" :class="{'bg-[#F6F6F7]': pageContext.urlPathname === link.path}"> {{ link.label }}</li>
+            <li
+v-for="link in links" :key="link.path" class="cursor-pointer py-1.5 px-4 rounded-[8rem]"
+                :class="{ 'bg-[#F6F6F7]': pageContext.urlPathname === link.path }">
+                <a :href="link.path">
+                    {{ link.label }}
+                </a>
+            </li>
         </ul>
         <AppButton class="!hidden md:!inline-flex">Send email</AppButton>
         <HamburgerMenu class="md:hidden" />
