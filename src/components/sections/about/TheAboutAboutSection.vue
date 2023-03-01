ca28ef037630f710bce360981427bd39f917ec72
Artur Burecki <a.burecki@standardweryfikacji.pl>
2023-03-01 21:44:33 +0100

    add dashed stokes to perks section

--- src/components/sections/about/TheAboutAboutSection.vue
+++ src/components/sections/about/TheAboutAboutSection.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
 import { SwiperSlide } from 'swiper/vue'
 import { useBreakpoints } from '@vueuse/core'
-import breakpointsConst from "~/constants/breakpoints";
+import breakpointsConst from '~/constants/breakpoints'
 
 import BottleIcon from '~/assets/icons/bottle-icon.svg?component'
-import TheAboutComponent from '~/components/about/TheAboutComponent.vue';
+import TheAboutComponent from '~/components/about/TheAboutComponent.vue'
 import box from '~/assets/box.jpeg'
 import convent from '~/assets/convent.jpeg'
 import about from '~/assets/about-image.png'
@@ -14,131 +14,140 @@ import BagIcon from '~/assets/icons/bag-icon.svg?component'
 import NicImage from '~/assets/employees/nic.jpeg'
 import EwaImage from '~/assets/employees/ewa.jpeg'
 
-const breakpoints =  useBreakpoints(breakpointsConst) 
+const breakpoints = useBreakpoints(breakpointsConst)
 const md = ref(breakpoints.smaller('md'))
 const details = [
-    {
-        img: about,
-        icon: RobeIcon,
-        header: 'Our headquarters',
-        desc: 'Meticulously designed according to our interior brand book. Always open for the team.'
-    },
-    {
-        img: NicImage,
-        icon: HoodieIcon,
-        header: 'Our team gadgets',
-        desc: 'Best local manufacturers and tons of love. That’s how our team gadgets are made.'
-    },
-    {
-        img: EwaImage,
-        icon: BagIcon,
-        header: 'Our team retreats',
-        desc: 'The best way to keep a remote team integrated and motivated. They’re an essential part of our culture.'
-    },
+  {
+    img: about,
+    icon: RobeIcon,
+    header: 'Our headquarters',
+    desc: 'Meticulously designed according to our interior brand book. Always open for the team.',
+  },
+  {
+    img: NicImage,
+    icon: HoodieIcon,
+    header: 'Our team gadgets',
+    desc: 'Best local manufacturers and tons of love. That’s how our team gadgets are made.',
+  },
+  {
+    img: EwaImage,
+    icon: BagIcon,
+    header: 'Our team retreats',
+    desc: 'The best way to keep a remote team integrated and motivated. They’re an essential part of our culture.',
+  },
 ]
 
 const selectedDetail = ref(details[0])
 
 // const breakpoints = useBreakpoints()
 
-
 const swiperOptions = computed(() => ({
-    slidesPerView: 'auto',
-    spaceBetween: 24,
-    navigation: false,
-    loop: true,
-    watchSlidesVisibility: true,
-    autoplay: {
-        delay: 2000,
-    },
+  slidesPerView: 'auto',
+  spaceBetween: 24,
+  navigation: false,
+  loop: true,
+  watchSlidesVisibility: true,
+  autoplay: {
+    delay: 2000,
+  },
 }))
 
 const content = [
-    {
-        title: 'We are value driven',
-        text: 'Our team is comprised of high-achieving individuals passionate about their craft and striving to be best at it.',
-        bgImage: `url(${convent})`
-    },
-    {
-        title: 'From a freelancing gig to a 17 people agency. Read now about how it all started.',
-    },
-    {
-        title: 'We’re building a home for designers',
-        text: 'Team retreats at Semiflat are legendary. As a remote team we get together every couple of months.',
-        bgImage: `url(${box})`
-    },
+  {
+    title: 'We are value driven',
+    text: 'Our team is comprised of high-achieving individuals passionate about their craft and striving to be best at it.',
+    bgImage: `url(${convent})`,
+  },
+  {
+    title: 'From a freelancing gig to a 17 people agency. Read now about how it all started.',
+  },
+  {
+    title: 'We’re building a home for designers',
+    text: 'Team retreats at Semiflat are legendary. As a remote team we get together every couple of months.',
+    bgImage: `url(${box})`,
+  },
 ]
 
 const changeImage = (card: typeof details[0]) => {
-    selectedDetail.value = card
-    document.querySelector('#detail-image')?.classList.remove('fade-animation')
-    setTimeout(() => document.querySelector('#detail-image')?.classList.add('fade-animation'), 0)
-
+  selectedDetail.value = card
+  document.querySelector('#detail-image')?.classList.remove('fade-animation')
+  setTimeout(() => document.querySelector('#detail-image')?.classList.add('fade-animation'), 0)
 }
-
-
 </script>
 
 <template>
-    <div class="page-content">
-        <TheAboutComponent :content="content" :icon="BottleIcon" />
-        <div class="mt-16">
-            <h2
-                class="text-xl gradient-text font-semibold mb-12 md:text-[1.75rem] md:leading-[120%] md:text-center md:mb-[5rem]">
-                Our attention to detail goes far beyond the design <br class="hidden md:block" /> work we do_</h2>
-            <div>
-                <img
-id="detail-image" :src="selectedDetail.img"
-                    class="rounded-4 h-[13.75rem] -scale-x-100 md:scale-x-100 md:w-full md:h-[30rem] mx-auto object-cover transition-all hidden sm:block">
-                <div>
-                    <AppSlider  v-if="md" class="mt-8" style="" :swiper-options="swiperOptions">
-                        <SwiperSlide v-for="(card, i) in details" :key="i">
-                            <img
-id="detail-image" :src="card.img"
-                                class="rounded-4 h-[13.75rem] -scale-x-100 md:scale-x-100 md:w-full md:h-[30rem] mx-auto object-cover transition-all">
-                            <div class="shrink-0 mx-auto flex items-center justify-center mt-8">
-                                <div
-                                    class="p-6 border border-[#BCDCDC] rounded-3 md:border-none max-w-[21.687rem] object-cover">
-                                    <component :is="card.icon" class="mb-8" />
-                                    <p class="mb-1 gradient-text font-semibold">{{ card.header }}</p>
-                                    <p class="text-blue-200">{{ card.desc }}</p>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                    </AppSlider>
-                    <div v-else class="shrink-0 mx-auto items-center justify-between hidden flex mt-10">
-                        <div
-v-for="(card, i) in details" :key="i"
-                            class="p-6 rounded-3 max-w-[21.687rem] object-cover cursor-pointer transition-all ease-linear duration-200 outline outline-1 outline-[#BCDCDC]/0 hover:outline-[#BCDCDC]/100"
-                            :class="{'!outline !outline-1 !outline-[#BCDCDC] shadow-select': selectedDetail.img === card.img}"
-                            @click="changeImage(card)">
-                            <component :is="card.icon" class="mb-8" />
-                            <p class="mb-1 gradient-text font-semibold">{{ card.header }}</p>
-                            <p class="text-blue-200">{{ card.desc }}</p>
-                        </div>
-                    </div>
+  <div class="page-content md:-mt-20">
+    <TheAboutComponent :content="content" :icon="BottleIcon" />
+    <div class="mt-16">
+      <h2
+        class="text-xl gradient-text font-semibold mb-12 md:text-[1.75rem] md:leading-[120%] md:text-center md:mb-[5rem]"
+      >
+        Our attention to detail goes far beyond the design <br class="hidden md:block" />
+        work we do_
+      </h2>
+      <div>
+        <img
+          id="detail-image"
+          :src="selectedDetail.img"
+          class="rounded-4 h-[13.75rem] -scale-x-100 md:scale-x-100 md:w-full md:h-[30rem] mx-auto object-cover transition-all hidden sm:block"
+        />
+        <div>
+          <AppSlider v-if="md" class="mt-8" style="" :swiper-options="swiperOptions">
+            <SwiperSlide v-for="(card, i) in details" :key="i">
+              <img
+                id="detail-image"
+                :src="card.img"
+                class="rounded-4 h-[13.75rem] -scale-x-100 md:scale-x-100 md:w-full md:h-[30rem] mx-auto object-cover transition-all"
+              />
+              <div class="shrink-0 mx-auto flex items-center justify-center mt-8">
+                <div
+                  class="p-6 border border-[#BCDCDC] rounded-3 md:border-none max-w-[21.687rem] object-cover"
+                >
+                  <component :is="card.icon" class="mb-8" />
+                  <p class="mb-1 gradient-text font-semibold">{{ card.header }}</p>
+                  <p class="text-blue-200">{{ card.desc }}</p>
                 </div>
+              </div>
+            </SwiperSlide>
+          </AppSlider>
+          <div v-else class="shrink-0 mx-auto items-center justify-between hidden flex mt-10">
+            <div
+              v-for="(card, i) in details"
+              :key="i"
+              class="p-6 rounded-3 max-w-[21.687rem] object-cover cursor-pointer transition-all ease-linear duration-200 outline outline-1 outline-[#BCDCDC]/0 hover:outline-[#BCDCDC]/100"
+              :class="{
+                '!outline !outline-1 !outline-[#BCDCDC] shadow-select':
+                  selectedDetail.img === card.img,
+              }"
+              @click="changeImage(card)"
+            >
+              <component :is="card.icon" class="mb-8" />
+              <p class="mb-1 gradient-text font-semibold">{{ card.header }}</p>
+              <p class="text-blue-200">{{ card.desc }}</p>
             </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
 @keyframes fade {
-    0% {
-        opacity: 0;
-    }
+  0% {
+    opacity: 0;
+  }
 
-    100% {
-        opacity: 100%;
-    }
+  100% {
+    opacity: 100%;
+  }
 }
 
 .fade-animation {
-    animation: fade 1s;
+  animation: fade 1s;
 }
 
 .shadow-select {
-    box-shadow: 0px 1px 0px rgba(17, 24, 28, 0.12), 0px 4px 8px rgba(0, 0, 0, 0.08);
+  box-shadow: 0px 1px 0px rgba(17, 24, 28, 0.12), 0px 4px 8px rgba(0, 0, 0, 0.08);
 }
 </style>
--- src/components/sections/about/TheAboutPerksSection.vue
+++ src/components/sections/about/TheAboutPerksSection.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { useBreakpoints } from '@vueuse/core'
 import ExpertiseIcon from '~/assets/icons/expertise.svg?component'
 import TalentIcon from '~/assets/icons/talent.svg?component'
 import LeadershipIcon from '~/assets/icons/leadership.svg?component'
@@ -8,10 +7,8 @@ import CirclesBackground from '~/assets/circles-bg1.svg?component'
 import CirclesBackground2 from '~/assets/circles-bg2.svg?component'
 import ExpertiseIllustration from '~/assets/illustrations/expertise-illustration.svg?component'
 import FlawlessIllustration from '~/assets/illustrations/flawless-illustration.svg?component'
-import breakpointsConst from '~/constants/breakpoints'
-
-const breakpoints = useBreakpoints(breakpointsConst)
-const md = ref(breakpoints.smaller('md'))
+import StrokeLeft from '~/assets/index-strokes/stroke-left.svg?component'
+import StrokeTop from '~/assets/index-strokes/stroke-top.svg?component'
 
 const perks = [
   {
@@ -47,33 +44,45 @@ const perks = [
       We design tools that help millions of professionals <br class="hidden md:block" />
       be more effective at their job_
     </h2>
-    <div v-if="md" class="grid gap-6 md:grid-cols-3 md:hidden">
+    <div class="grid gap-6 md:grid-cols-3 md:hidden">
       <ThePerkComponent v-for="(perk, i) in perks" :key="i" :perk="perk" />
       <div class="px-8 py-10 bg-dark-blue rounded-4 relative md:static">
         <p class="text-white text-xl mb-8 font-semibold">
           Want to see how we apply these in practice?
         </p>
         <AppButton secondary href="/work">See our work</AppButton>
-        <CirclesBackground class="absolute bottom-0 right-0" />
+        <AppCirclesHorizontal id="circles-sm" class="absolute right-0 bottom-0 h-51" />
       </div>
     </div>
-    <div v-else class="hidden md:flex gap-8">
-      <div class="flex flex-col gap-8 max-w-[346px] justify-center">
+  </div>
+  <div
+    class="relative mask-testimonial pt-5rem pb-5rem max-w-86rem mx-auto hidden md:block px-6 lg:px-0"
+  >
+    <div class="hidden md:flex gap-8 max-w-69rem mx-auto">
+      <div class="flex flex-col gap-8 max-w-[346px] justify-center relative">
+        <component :is="StrokeTop" class="absolute w-full -top-33" />
+        <component :is="StrokeLeft" class="absolute w-full -left-[calc(100%+2rem)]" />
+        <component :is="StrokeTop" class="absolute w-full -bottom-33 -scale-y-100" />
         <ThePerkComponent :perk="perks[0]" />
       </div>
-      <div class="flex flex-col gap-8 max-w-[346px]">
+      <div class="flex flex-col gap-8 max-w-[346px] relative">
+        <component :is="StrokeTop" class="absolute w-full -top-62" />
         <ThePerkComponent :perk="perks[1]" />
         <div class="px-8 py-10 bg-dark-blue rounded-4 relative">
           <p class="text-white text-xl mb-8 font-semibold">
             Want to see how we apply these in practice?
           </p>
           <AppButton secondary href="/work">See our work</AppButton>
-          <CirclesBackground2 class="absolute bottom-0 right-0" />
+          <AppCirclesHorizontal id="circles-md" class="absolute right-0 bottom-0 h-51" />
         </div>
         <ThePerkComponent :perk="perks[2]" />
+        <component :is="StrokeTop" class="absolute w-full -bottom-62 -scale-y-100" />
       </div>
-      <div class="flex flex-col gap-8 max-w-[346px] justify-center">
+      <div class="flex flex-col gap-8 max-w-[346px] justify-center relative">
+        <component :is="StrokeTop" class="absolute w-full -top-33" />
+        <component :is="StrokeLeft" class="absolute w-full -right-[calc(100%+2rem)] -scale-x-100" />
         <ThePerkComponent :perk="perks[3]" />
+        <component :is="StrokeTop" class="absolute w-full -bottom-33 -scale-y-100" />
       </div>
     </div>
   </div>
@@ -84,5 +93,13 @@ const perks = [
   background: radial-gradient(57.03% 76.98% at 100% 0%, #ffffff 0%, rgba(255, 255, 255, 0) 100%),
     #031c26;
   background-blend-mode: overlay, normal;
+}
+
+.mask-testimonial {
+  -webkit-mask-image: radial-gradient(
+    50% 46% at 50% 50%,
+    #d9d9d9 84.46%,
+    rgba(217, 217, 217, 0) 122%
+  );
 }
 </style>
